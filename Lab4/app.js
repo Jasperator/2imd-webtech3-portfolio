d7084243164d1ae8866e2efe648f9e09f435beca
Jasperator <55495886+Jasperator@users.noreply.github.com>
2020-03-27 10:23:44 +0100

    Trying to add sperhero API but API does not respond

--- Lab4/app.js
+++ Lab4/app.js
@@ -1,95 +1,117 @@
 class App {
-    constructor(){
-       this.getLocation();
-       this.lat;
-       this.lng;
-       this.bat;
+    constructor() {
+        this.getLocation();
+        this.lat;
+        this.lng;
+        this.bat;
     }
 
-    getLocation(){
+    getLocation() {
         navigator.geolocation.getCurrentPosition(
-        this.gotLocation.bind(this),
-        this.errorLocation.bind(this)
+            this.gotLocation.bind(this),
+            this.errorLocation.bind(this)
         );
     }
 
-    gotLocation(result){
-       this.lat = result.coords.latitude;
-       this.lng = result.coords.longitude;
-       this.getWeather();
+    gotLocation(result) {
+        this.lat = result.coords.latitude;
+        this.lng = result.coords.longitude;
+        this.getWeather();
     }
 
- 
 
-
-
-
-
-    getWeather(){
-        // https://api.darksky.net/forecast/4bc0f32cb110e0f62f38efb677cfebcc/37.8267,-122.4233
-        let url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/4bc0f32cb110e0f62f38efb677cfebcc/${this.lat}, ${this.lng}?units=si`;
-
+    getsuperhero() {
+        let url = `https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/2952368384829938/339`;
         fetch(url)
-        .then(response => {
-           return response.json();
-        }).then(data => {
-            if(data.currently.temperature < 5) {
-                var goodweather = document.getElementById("goodweather");
-                goodweather.style.display = "none";
-
-            document.querySelector("#inside2").innerHTML = data.currently.summary;
-            document.querySelector("#inside").innerHTML = data.currently.temperature;
-
-            navigator.battery || navigator.mozBattery || navigator.webkitBattery;
-
+        .then(response =>{
+             console.log(response);
+            return response.json();
          
-            navigator.getBattery().then(function(battery) {
-                function updateAllBatteryInfo(){
-                  updateLevelInfo();
-
-                }
-                updateAllBatteryInfo();
-
-                function updateLevelInfo(){
-                              document.querySelector("#battery").innerHTML =  (battery.level * 100 + "%");
-                }
-
-            }
-        
-
-             ) } else {
-
-                var badweather = document.getElementById("badweather");
-                badweather.style.display = "none";
                 
+            }).then(data => {
+                document.querySelector("#superhero").innerHTML = data.name;
+                document.querySelector("#superhero2").innerHTML = data.name;
 
-                document.querySelector("#outside2").innerHTML = data.currently.summary;
-                document.querySelector("#outside").innerHTML = data.currently.temperature;
-
-
-                navigator.getBattery().then(function(battery) {
-                    function updateAllBatteryInfo(){
-                      updateLevelInfo();
-    
-                    }
-                    updateAllBatteryInfo();
-    
-                    function updateLevelInfo(){
-                                  document.querySelector("#battery2").innerHTML =  (battery.level * 100 + "%");
-                    }
-    
-                })
-            }
-            
-        }) .catch(err=>{
-            console.log(err);
-        })
+                
+            }).catch(err =>{
+                console.log(err);
+            });
     }
 
 
-    errorLocation(err){
+
+
+    getWeather() {
+        // https://api.darksky.net/forecast/4bc0f32cb110e0f62f38efb677cfebcc/37.8267,-122.4233
+        let url2 = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/4bc0f32cb110e0f62f38efb677cfebcc/${this.lat}, ${this.lng}?units=si`;
+
+
+
+        fetch(url2)
+            .then((response) => {
+                console.log(response);
+                return response.json();
+            }).then(data => {
+                if (data.currently.temperature < 5) {
+                    var goodweather = document.getElementById("goodweather");
+                    goodweather.style.display = "none";
+
+                    document.querySelector("#inside2").innerHTML = data.currently.summary;
+                    document.querySelector("#inside").innerHTML = data.currently.temperature;
+
+                    navigator.battery || navigator.mozBattery || navigator.webkitBattery;
+
+
+                    navigator.getBattery().then(function (battery) {
+                        function updateAllBatteryInfo() {
+                            updateLevelInfo();
+
+                        }
+                        updateAllBatteryInfo();
+
+                        function updateLevelInfo() {
+                            document.querySelector("#battery").innerHTML = (battery.level * 100 + "%");
+                        }
+
+                    }
+
+
+                    )
+                } else {
+
+                    var badweather = document.getElementById("badweather");
+                    badweather.style.display = "none";
+
+
+                    document.querySelector("#outside2").innerHTML = data.currently.summary;
+                    document.querySelector("#outside").innerHTML = data.currently.temperature;
+
+
+                    navigator.getBattery().then(function (battery) {
+                        function updateAllBatteryInfo() {
+                            updateLevelInfo();
+
+                        }
+                        updateAllBatteryInfo();
+
+                        function updateLevelInfo() {
+                            document.querySelector("#battery2").innerHTML = (battery.level * 100 + "%");
+                        }
+
+                    })
+                }
+
+            }).catch(err => {
+                console.log(err);
+            })
+    }
+
+
+    errorLocation(err) {
         console.log(err);
     }
 }
+
+
 
 let app = new App();
